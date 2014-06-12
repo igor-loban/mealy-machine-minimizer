b95a29de04f9cf364ab54cb080c41e43c284a2a7
i.loban <i.loban@itransition.com>
2014-06-12 11:47:14 +0300

    markup framework added

--- src/main/java/by/bsu/fpmi/memami/WizardBean.java
+++ src/main/java/by/bsu/fpmi/memami/WizardBean.java
@@ -5,7 +5,7 @@ import javax.faces.bean.SessionScoped;
 
 @ManagedBean
 @SessionScoped
-public class TestBean {
+public class WizardBean {
     public String getText() {
         return "Hello, world!";
     }
